cf5c0c29b8a65e0c634e56a837f8130ba7d0165f
jelaiw <14236583+jelaiw@users.noreply.github.com>
2024-10-03 23:08:58 +0000

    Credit to more experienced Rust programmers sharing their expertise.

--- set_1/challenge_1/src/main.rs
+++ set_1/challenge_1/src/main.rs
@@ -5,6 +5,7 @@ fn main() {
     let input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
     let raw_bytes = hex::decode(input).unwrap();
 
+    // https://stackoverflow.com/a/19076878. Note, Vec coerces to slices and other juicy tidbits.
     let m = str::from_utf8(&raw_bytes).unwrap();
     println!("{:?}", m);
 }
