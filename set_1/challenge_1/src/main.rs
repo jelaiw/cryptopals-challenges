9cab67f8ba51587a0400a4053abe9bbc3ba5be06
jelaiw <14236583+jelaiw@users.noreply.github.com>
2024-10-03 22:54:00 +0000

    Parse hex input to raw bytes.

---
+++ set_1/challenge_1/src/main.rs
@@ -0,0 +1,10 @@
+use hex;
+use std::str;
+
+fn main() {
+    let input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+    let raw_bytes = hex::decode(input).unwrap();
+
+    let m = str::from_utf8(&raw_bytes).unwrap();
+    println!("{:?}", m);
+}
