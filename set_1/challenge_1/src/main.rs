8a71d918aedc00f3da8e1feaef281c33b63a629e
jelaiw <14236583+jelaiw@users.noreply.github.com>
2024-10-06 22:54:06 +0000

    Annotate types at variable binding, which may help a bit with the learning curve.

--- set_1/challenge_1/src/main.rs
+++ set_1/challenge_1/src/main.rs
@@ -3,13 +3,13 @@ use std::str;
 use base64::{engine::general_purpose::STANDARD, Engine as _};
 
 fn main() {
-    let input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
-    let raw_bytes = hex::decode(input).unwrap();
+    let input: &str = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+    let raw_bytes: Vec<u8> = hex::decode(input).unwrap();
 
     // https://stackoverflow.com/a/19076878. Note, Vec coerces to slices and other juicy tidbits.
-    let m = str::from_utf8(&raw_bytes).unwrap();
+    let m: &str = str::from_utf8(&raw_bytes).unwrap();
     println!("{:?}", m);
 
-    let b64 = STANDARD.encode(raw_bytes);
+    let b64: String = STANDARD.encode(raw_bytes);
     println!("{}", b64);
 }
